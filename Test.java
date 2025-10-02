76ede33a6e90a38632bb2a837f6ccd44eb5fabce
Maulana Al Jabbar.Z <mlana.alj22@gmail.com>
2025-10-02 15:35:16 +0700

    new3

--- Test.java
+++ Test.java
@@ -3,5 +3,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris pertama saya");
         System.out.println("user 27 menambahkan baris baru");
+        double a = 56;
+        double b = 43;
+
+        double hasil = a * b;
     }
 }
